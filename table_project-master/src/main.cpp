13debf396f950ce079b5bb3f75631e891238f579
jayson <jayson.kaced@gmail.com>
2017-11-06 20:05:42 +0100

    Suppression / Recherche (75%)

--- table_project-master/src/main.cpp
+++ table_project-master/src/main.cpp
@@ -11,14 +11,13 @@ int main(){
 	Table a;
 
 	a.rempTab(); //// Tu peux tester ça
-
-
 	a.affiche_tab();
+	Elem c(1360325192, 24);
+	Elem s(1144433223, 47);
+	cout << "Test de la recherche" << endl;
 
-	cout<<"yo"<<endl; 
-
-
-
+	a.recherche2(c);
+	a.recherche2(s);
 
 	return 0;
 }
